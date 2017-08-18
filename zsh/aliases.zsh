2c678d9eca22d1b612982a60adabf482bc6b1aef
Benjamin Denhartog <ben@bddenhartog.com>
2017-08-18 12:20:20 -0700

    remove redundant aliases `wb` and `wg` this functionality (and more) is provided by the `wo` function

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,17 +1,11 @@
 #!/usr/bin/env zsh
 
-# aliases.zsh: Useful aliases for working in the terminal
-
 # Generic aliases
 alias l="ls --color=tty --group-directories-first -AlFh"
 alias ll="ls --color=tty --group-directories-first -AlFhL"
 alias reload=". ~/.zshrc"
 alias cls="clear"
 alias ...=../..
-
-# Environment-specific
-alias wg="cd ${PROJECT_HOME}/github.com"
-alias wb="cd ${PROJECT_HOME}/bitbucket.org"
 
 # Application-specific
 alias gulp="./node_modules/.bin/gulp"
