a29bf718c2f5d5a7d806df9f2153b7cfe6a42712
Benjamin Denhartog <ben@bddenhartog.com>
2018-01-14 22:33:15 -0800

    remove unecessary special function bodies

--- zsh/window.zsh
+++ zsh/window.zsh
@@ -2,11 +2,7 @@
 
 case $TERM in
     *xterm*|*rxvt*|*screen*)
-        # Special function precmd, executed before displaying each prompt.
         function precmd() {
-            # Set the terminal title to the current working directory.
-            print -Pn "\e]0;%n@%m:%~\a"
-
             # Print a newline before the prompt, unless it's the first
             # prompt in the parent process.
             if [ -z "$NEW_LINE_BEFORE_PROMPT" ]; then
@@ -14,11 +10,5 @@ case $TERM in
             elif [ "$NEW_LINE_BEFORE_PROMPT" -eq 1 ]; then
                 print ""
             fi
-        }
-
-        # Special function preexec, executed before running each command.
-        function preexec() {
-            # Set the terminal title to the curently running command.
-            print -Pn "\e]2;[${2:q}]\a"
         }
 esac
