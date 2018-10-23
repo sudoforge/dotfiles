97ebc0b135e98884032b1b9439955f3cafa088a4
Benjamin Denhartog <ben@sudoforge.com>
2018-10-22 17:33:31 -0700

    update git path abbreviation

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -6,7 +6,7 @@ __current_dir() {
         -e 's;'"${code_dir}"'/github.com/(.+);gh::\1;' \
         -e 's;'"${code_dir}"'/bitbucket.org/(.+);bb::\1;' \
         -e 's;'"${code_dir}"'/aur.archlinux.org/(.+);aur::\1;' \
-        -e 's;'"${code_dir}"'/github.platforms.engineering/(.+);mongit::\1;' \
+        -e 's;'"${code_dir}"'/github.platforms.engineering/(.+);baygit::\1;' \
         -e 's;'"${HOME}"';\~;'
 }
 
