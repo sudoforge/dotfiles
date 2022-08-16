f7facf2ecc18c6438a55c7ba7e7e6e96093b869c
sudoforge <9c001b67637a@sudoforge.com>
2022-08-16 13:52:19 -0600

    feat: add conditionals for language server configurations
    Change-Id: Ic6b0d0bc49f06b61deb946d82a37ed09145418ce
    GitOrigin-RevId: 7251d335a36a9be91223fef5d5fc8ddd0420f920

--- nvim/.config/nvim/lua/lsp.lua
+++ nvim/.config/nvim/lua/lsp.lua
@@ -24,80 +24,92 @@ local lspconfig = require("lspconfig")
 local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 -- Rust
-require("rust-tools").setup {
-	capabilities = capabilities,
-	on_attach = on_attach,
+if vim.fn.executable("rust-analyzer") == 1 then
+	require("rust-tools").setup {
+		capabilities = capabilities,
+		on_attach = on_attach,
 
-	tools = {
-		autoSetHints = true,
-		hover_with_actions = true,
-		inlay_hints = {
-			show_parameter_hints = false,
-			parameter_hints_prefix = "",
-			other_hints_prefix = "",
+		tools = {
+			autoSetHints = true,
+			hover_with_actions = true,
+			inlay_hints = {
+				show_parameter_hints = false,
+				parameter_hints_prefix = "",
+				other_hints_prefix = "",
+			},
 		},
-	},
 
-	server = {
-		settings = {
-			["rust-analyzer"] = {
-				checkOnSave = {
-					command = "clippy",
+		server = {
+			settings = {
+				["rust-analyzer"] = {
+					checkOnSave = {
+						command = "clippy",
+					},
 				},
 			},
 		},
-	},
-}
+	}
+end
 
 -- Golang
-lspconfig.gopls.setup {
-	cmd = { "gopls" },
-	capabilities = capabilities,
-	filetypes = { "go", "gomod" },
-	on_attach = on_attach,
-}
+if vim.fn.executable("gopls") == 1 then
+	lspconfig.gopls.setup {
+		cmd = { "gopls" },
+		capabilities = capabilities,
+		filetypes = { "go", "gomod" },
+		on_attach = on_attach,
+	}
+end
 
 -- Java
-lspconfig.java_language_server.setup {
-	cmd = { "java-language-server" },
-	capabilities = capabilities,
-	filetypes = { "java" },
-	on_attach = on_attach,
-}
+if vim.fn.executable("java-language-server") then
+	lspconfig.java_language_server.setup {
+		cmd = { "java-language-server" },
+		capabilities = capabilities,
+		filetypes = { "java" },
+		on_attach = on_attach,
+	}
+end
 
 -- Lua
-lspconfig.sumneko_lua.setup {
-	cmd = { "/usr/bin/lua-language-server" },
-	capabilities = capabilities,
-	settings = {
-		Lua = {
-			runtime = {
-				version = "LuaJIT",
-			},
-			diagnostics = {
-				globals = { "vim" },
-			},
-			workspace = {
-				library = vim.api.nvim_get_runtime_file("", true),
-			},
-			telemetry = {
-				enable = false,
+if vim.fn.executable("lua-language-server") then
+	lspconfig.sumneko_lua.setup {
+		cmd = { "/usr/bin/lua-language-server" },
+		capabilities = capabilities,
+		settings = {
+			Lua = {
+				runtime = {
+					version = "LuaJIT",
+				},
+				diagnostics = {
+					globals = { "vim" },
+				},
+				workspace = {
+					library = vim.api.nvim_get_runtime_file("", true),
+				},
+				telemetry = {
+					enable = false,
+				},
 			},
 		},
-	},
-	on_attach = on_attach,
-}
+		on_attach = on_attach,
+	}
+end
 
 -- Python
-lspconfig.pyright.setup {
-	cmd = { "pyright-langserver", "--stdio" },
-	capabilities = capabilities,
-	on_attach = on_attach,
-}
+if vim.fn.executable("pyright-langserver") then
+	lspconfig.pyright.setup {
+		cmd = { "pyright-langserver", "--stdio" },
+		capabilities = capabilities,
+		on_attach = on_attach,
+	}
+end
 
 -- Terraform
-lspconfig.terraformls.setup {
-	cmd = { "terraform-ls", "serve" },
-	capabilities = capabilities,
-	on_attach = on_attach,
-}
+if vim.fn.executable("terraform-ls") then
+	lspconfig.terraformls.setup {
+		cmd = { "terraform-ls", "serve" },
+		capabilities = capabilities,
+		on_attach = on_attach,
+	}
+end
