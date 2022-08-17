a2a63a6e31633d928124a477cc6b357760ef7b1a
sudoforge <9c001b67637a@sudoforge.com>
2022-08-16 18:12:57 -0600

    feat: normalize navigation and manipulation of quickfix lists
    Change-Id: Ia2504552c39a86133c15afc821bb7984dd348837
    GitOrigin-RevId: 1e91a78db2280fc41467e92db1a750f4578ce752

--- nvim/.config/nvim/ftplugin/qf.lua
+++ nvim/.config/nvim/ftplugin/qf.lua
@@ -3,11 +3,22 @@ if vim.b.did_local_ftplugin == true then
 	return 0
 end
 
--- Navigate with C-j and C-k
+-- Navigate windows with C-j and C-k
 -- These are default bindings, but get overriding by the system-level ftplugin
 vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", { noremap = true })
 vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", { noremap = true })
 
+-- More natural quickfix list navigation
+vim.api.nvim_set_keymap("n", "<C-n>", ":cnext<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-n>", ":cnext<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-p>", ":cprev<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<S-g>", ":clast<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "gg", ":cfirst<CR>", { noremap = true, silent = true })
+
+-- Override the default <leader>bd behavior
+-- <leader>bd is habitually used to close buffers but keep the split open, but
+-- when closing the quickfix list, it is desirable to close its split.
+vim.api.nvim_set_keymap("n", "<leader>bd", ":cclose<CR>", { noremap = true, silent = true })
 
 -- Signal that the plugin has completed
 vim.b.did_local_ftplugin = true
