b15990cb276d7b1bb7b3c5bb07e85f10e0d74ae3
sudoforge <9c001b67637a@sudoforge.com>
2022-11-23 00:35:25 -0700

    feat: change desktop background
    Change-Id: Ide2f7967b4bce96d3cb5358126b7f9829f705a47
    GitOrigin-RevId: f5643286602420e212896ab7382e68b7dc5c7b4d

--- autorandr/.config/autorandr/laptop/postswitch.d/set-background-image.bash
+++ autorandr/.config/autorandr/laptop/postswitch.d/set-background-image.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-# Set the background to a 1920x1080 image
-feh --no-fehbg --bg-center /usr/share/backgrounds/space.jpg
+# Set the background image
+feh --no-fehbg --bg-center /usr/share/backgrounds/abstract-dark-1920x1200.jpg
