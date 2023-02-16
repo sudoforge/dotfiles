3737918ffe9450ba427c6dd094b265f255e7d132
sudoforge <9c001b67637a@sudoforge.com>
2023-02-16 11:52:54 +0100

    fix: change class name for filepicker window
    Change-Id: I10812dfdc8952e5de8581ce3a58b573a2d9304e1
    GitOrigin-RevId: b1378d5c2eb82974e6958372c13e3b6deeaa57cf

--- autorandr/.config/autorandr/docked/postswitch.d/bspwm-rules.bash
+++ autorandr/.config/autorandr/docked/postswitch.d/bspwm-rules.bash
@@ -3,4 +3,4 @@
 # This file contains BSPC commands to create rules for nodes when my workstation
 # is docked and using an external display.
 
-bspc rule -a Alacritty:filepicker:\* state=floating rectangle=1000x600+780+420
+bspc rule -a filepicker:filepicker:\* state=floating rectangle=1000x600+780+420
--- autorandr/.config/autorandr/laptop/postswitch.d/bspwm-rules.bash
+++ autorandr/.config/autorandr/laptop/postswitch.d/bspwm-rules.bash
@@ -3,4 +3,4 @@
 # This file contains BSPC commands to create rules for nodes when connected to
 # my machine's native display.
 
-bspc rule -a Alacritty:filepicker:\* state=floating rectangle=1000x600+460+240
+bspc rule -a filepicker:filepicker:\* state=floating rectangle=1000x600+460+240
